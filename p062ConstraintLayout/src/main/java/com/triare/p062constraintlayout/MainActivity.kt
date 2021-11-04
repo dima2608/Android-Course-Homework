7b63ae0b9579fef9cc66d2e73c6b18ac1f5754a7
Dima Danilov <ddvdani14@gmail.com>
2021-11-04 14:12:27 +0200

    added main memu

--- p062ConstraintLayout/src/main/java/com/triare/p062constraintlayout/MainActivity.kt
+++ p062ConstraintLayout/src/main/java/com/triare/p062constraintlayout/MainActivity.kt
@@ -2,18 +2,34 @@ package com.triare.p062constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val nextLayout = findViewById<ImageButton>(R.id.similar)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_manu, menu)
+        return true
+    }
 
-        nextLayout.setOnClickListener {
-            setContentView(R.layout.andr_std)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu_btn_previous_layout -> {
+                setContentView(R.layout.activity_main)
+                Toast.makeText(applicationContext, "First layout", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_btn_next_layout -> {
+                setContentView(R.layout.andr_std)
+                Toast.makeText(applicationContext, "Second layout", Toast.LENGTH_SHORT).show()
+            }
         }
+        return super.onOptionsItemSelected(item)
     }
 }
