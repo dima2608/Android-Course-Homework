c9572cb361f3eb5e59915294fc63e9272cddc772
Dima Danilov <ddvdani14@gmail.com>
2021-12-18 12:50:14 +0200

    add app bar

--- p102quakealertapp/src/main/java/com/triare/p102quakealertapp/MainActivity.kt
+++ p102quakealertapp/src/main/java/com/triare/p102quakealertapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.triare.p102quakealertapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var navController: NavigationView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     }
 }
